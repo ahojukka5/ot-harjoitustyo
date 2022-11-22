4b47d2efcbd9e445972a7a1b3da5c6d811619e69
Jukka Aho <ahojukka5@gmail.com>
2022-11-22 20:28:56 +0200

    Implement get_cheapest_hour

--- src/saehaekkae.py
+++ src/saehaekkae.py
@@ -3,6 +3,8 @@
 
 from record import Record
 from database import Database
+from utils import get_cheapest_hour
+
 
 def main():
     return 0
---
+++ src/utils.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def get_cheapest_hour(database):
+    cheapest_record = None
+    cheapest_price = 2**32
+    records = database.get_records()
+    assert len(records) > 0
+    for record in records:
+        if record.get_energy_price() < cheapest_price:
+            cheapest_record = record
+            cheapest_price = record.get_energy_price()
+    return cheapest_record
