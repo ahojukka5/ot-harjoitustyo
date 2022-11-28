46a193adc3ad2a1d7040eef5b05330aa4407dd10
Jukka Aho <ahojukka5@gmail.com>
2022-11-28 22:58:24 +0200

    Update GUI

--- src/start_gui.py
+++ src/start_gui.py
@@ -1,10 +1,36 @@
-from tkinter import Tk
+import tkinter as tk
+import matplotlib
+import datasources
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+matplotlib.use("TkAgg")
+
+
+class App(tk.Tk):
+    def __init__(self, source="test"):
+        super().__init__()
+        data = datasources.fetch(source)
+        x = [d["DateTime"] for d in data]
+        y = [100.0 * d["PriceWithTax"] for d in data]
+
+        self.title("Saehaekkae - saehkoen saeaelimaetoen saeaestaejae!")
+        figure = Figure(figsize=(18, 8), dpi=100)
+        figure_canvas = FigureCanvasTkAgg(figure, self)
+        # NavigationToolbar2Tk(figure_canvas, self)
+        axes = figure.add_subplot()
+        axes.bar(x, y)
+        axes.set_title("Pörssisähkön hinta tunneittain")
+        axes.set_ylabel("Hinta (c/kWh)")
+        figure.autofmt_xdate(rotation=45)
+        figure.tight_layout()
+        figure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 
 def main():
-    window = Tk()
-    window.title("Saehaekkae - saehkoen saeaelimaetoen saeaestaejae!")
-    window.mainloop()
+    app = App()
+    app.mainloop()
 
 
 if __name__ == "__main__":
