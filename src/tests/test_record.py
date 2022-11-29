003ac7d5276302e2faace443694b063dbf622e6d
Jukka Aho <ahojukka5@gmail.com>
2022-11-29 20:13:33 +0200

    Add test for Record constructor

--- src/tests/test_record.py
+++ src/tests/test_record.py
@@ -3,6 +3,10 @@ from saehaekkae import Record
 
 
 class TestRecord(unittest.TestCase):
+    def test_record_constructor(self):
+        record = Record("2022-11-29 19:00", 10.0)
+        self.assertEqual(10.0, record.energy_price)
+
     def test_record_getters(self):
         record = Record(3, 20.0)
         self.assertEqual(3, record.get_time())
