ddb71e2d6d1851a444899396de452b95a888be78
Jukka Aho <ahojukka5@gmail.com>
2022-11-29 20:14:30 +0200

    Add test for Database.to_dataframe

--- src/tests/test_database.py
+++ src/tests/test_database.py
@@ -1,6 +1,8 @@
 import unittest
 from saehaekkae import Database, Record
 
+import pandas as pd
+
 
 class TestDatabase(unittest.TestCase):
     def test_database(self):
@@ -10,3 +12,15 @@ class TestDatabase(unittest.TestCase):
         records = db.get_records()
         first_record = records[0]
         self.assertEqual(record, first_record)
+
+    def test_to_dataframe(self):
+        db = Database()
+        db.add_record(Record(pd.to_datetime("2022-11-28T00:00:00"), 10.0))
+        db.add_record(Record(pd.to_datetime("2022-11-28T01:00:00"), 20.0))
+        df = db.to_dataframe()
+        print(df)
+        row1 = df.iloc[0]
+        row2 = df.iloc[1]
+        self.assertEqual(2, len(df))
+        self.assertEqual(10.0, row1["energy_price"])
+        self.assertEqual(pd.to_datetime("2022-11-28T01:00:00"), row2.name)
