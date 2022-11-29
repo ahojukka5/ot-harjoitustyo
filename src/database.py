acb81daa600e89beb4c29d805d487214a2ec923d
Jukka Aho <ahojukka5@gmail.com>
2022-11-29 20:12:27 +0200

    Implement database export to dataframe

--- src/database.py
+++ src/database.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 class Database:
     def __init__(self):
         self.records = []
@@ -7,3 +10,10 @@ class Database:
 
     def get_records(self):
         return self.records
+
+    def to_dataframe(self):
+        data = [record.to_dict() for record in self.get_records()]
+        dataframe = pd.DataFrame(data)
+        dataframe.index = dataframe["time"]
+        del dataframe["time"]
+        return dataframe
