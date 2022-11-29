0bf46f4e9b0b70c33d864c01a1bd544d1bc6d20f
Jukka Aho <ahojukka5@gmail.com>
2022-11-29 20:15:14 +0200

    datasources.fetch(): return Database

--- src/datasources.py
+++ src/datasources.py
@@ -1,6 +1,18 @@
+from database import Database
+from record import Record
+
+import pandas as pd
+
 from tests import TEST_DATA
 
 
 def fetch(source):
+    database = Database()
     if source == "test":
-        return TEST_DATA
+        for row in TEST_DATA:
+            time = pd.to_datetime(row["DateTime"]).to_pydatetime()
+            price = row["PriceNoTax"]
+            record = Record(time, price)
+            database.add_record(record)
+        return database
+    raise NotImplementedError(f"data source {source} not implemented")
