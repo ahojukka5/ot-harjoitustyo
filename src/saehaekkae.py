c4dd2aa3ec42f147e9758f581a4def71abc820ca
Jukka Aho <ahojukka5@gmail.com>
2022-12-06 20:07:48 +0200

    Ignore pylint errors on this file with a valid reason

--- src/saehaekkae.py
+++ src/saehaekkae.py
@@ -1,3 +1,8 @@
+# pylint: skip-file
+
+# A very justified reason for using the pylint disable command is that this way
+# the user can more easily import the functions of the package.
+
 from record import Record
 from database import Database
 from utils import get_cheapest_hour
