fda7a539a228d21836418ce92b46c65f735e63f1
Jukka Aho <ahojukka5@gmail.com>
2022-12-13 20:55:15 +0200

    Add settings for consumption data

--- src/settings.py
+++ src/settings.py
@@ -11,5 +11,11 @@ ENERGY_PRICE_SOURCE = "test"
 ENERGY_PRICE_URI = "https://api.spot-hinta.fi/TodayAndDayForward"
 ENERGY_PRICE_FILE = "data/energy-prices.json"
 
+ENERGY_CONSUMPTION_SOURCE = "test"
+ENERGY_CONSUMPTION_FILE = "data/energy-consumption.csv"
+
+if "SAEHAEKKAE_ENERGY_CONSUMPTION_FILE" in os.environ:
+    ENERGY_CONSUMPTION_FILE = os.environ["SAEHAEKKAE_ENERGY_CONSUMPTION_FILE"]
+
 if "SAEHAEKKAE_ENERGY_PRICE_FILE" in os.environ:
     ENERGY_PRICE_FILE = os.environ["SAEHAEKKAE_ENERGY_PRICE_FILE"]
