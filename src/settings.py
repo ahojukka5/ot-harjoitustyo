130135922aa8d60d8bfdffdee524c5ff4a9e83b2
Jukka Aho <ahojukka5@gmail.com>
2022-12-13 18:03:28 +0200

    Add simple settings file

---
+++ src/settings.py
@@ -0,0 +1,15 @@
+"""Application settings.
+
+Application settings be set using environment variables. Some meaningful
+defaults are provided.
+"""
+
+import os
+
+# Options, "test", "local", "internet"
+ENERGY_PRICE_SOURCE = "local"
+ENERGY_PRICE_URI = "https://api.spot-hinta.fi/TodayAndDayForward"
+ENERGY_PRICE_FILE = "data/energy-prices.json"
+
+if "SAEHAEKKAE_ENERGY_PRICE_FILE" in os.environ:
+    ENERGY_PRICE_FILE = os.environ["SAEHAEKKAE_ENERGY_PRICE_FILE"]
