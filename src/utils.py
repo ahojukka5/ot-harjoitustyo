2f81a9482812e3780a146f3b33424ce0714397d5
Jukka Aho <ahojukka5@gmail.com>
2022-12-13 17:17:58 +0200

    Add documentation for utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,16 @@
+"""Utility functions and helpers to be used internally.
+"""
+
+
 def get_cheapest_hour(database):
+    """Return the cheapest hour from the database.
+
+    Args:
+        database: a non-empty Database object.
+
+    Returns:
+        A Record where energy price is cheapest.
+    """
     cheapest_record = None
     cheapest_price = 2**32
     records = database.get_records()
