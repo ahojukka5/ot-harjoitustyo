851e8c883648788af267bd519bb18474c1fa9212
Jukka Aho <ahojukka5@gmail.com>
2022-12-16 15:06:25 +0200

    Add test for Database.from_dataframe

--- src/tests/test_database.py
+++ src/tests/test_database.py
@@ -26,3 +26,11 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(10.0, row1["price"])
         print("type of row2.name is ", type(row2.name))
         self.assertEqual(pd.Timestamp("2022-11-28T01:00:00Z"), row2.name)
+
+    def test_from_dataframe(self):
+        t1 = parser.parse("2022-12-16T15:00:00")
+        t2 = parser.parse("2022-12-16T16:00:00")
+        df = pd.DataFrame({"price": [1, 2], "amount": [3, 4]}, index=[t1, t2])
+        db = Database.from_dataframe(df)
+        recs = db.get_records()
+        self.assertEqual(1.0, recs[0].get_price())
