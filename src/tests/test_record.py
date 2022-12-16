42a60ad769594a8292930c2b859e78c62c336050
Jukka Aho <ahojukka5@gmail.com>
2022-12-16 14:37:14 +0200

    Update tests for record

--- src/tests/test_record.py
+++ src/tests/test_record.py
@@ -1,17 +1,39 @@
 import unittest
+import pytz
+from datetime import datetime
 from saehaekkae import Record
 
 
 class TestRecord(unittest.TestCase):
+    def setUp(self):
+        self.record = Record("2022-12-16T21:00:00Z", price=10.0, amount=3.0)
+
     def test_record_constructor(self):
-        record = Record("2022-11-29 19:00", 10.0)
-        self.assertEqual(10.0, record.energy_price)
+        record1 = Record(1671224400, price=10.0, amount=3.0)
+        record2 = Record("2022-12-16T21:00:00Z", price=10.0, amount=3.0)
+        record3 = Record(datetime(2022, 12, 16, 21, 0, 0), price=10.0, amount=3.0)
+        print(record1)
+        print(record2)
+        print(record3)
+        self.assertEqual(record1, record2)
+        self.assertEqual(record2, record3)
 
     def test_record_getters(self):
-        record = Record(3, 20.0)
-        self.assertEqual(3, record.get_time())
-        self.assertEqual(20.0, record.get_energy_price())
+        record = self.record
+        self.assertEqual(
+            datetime(2022, 12, 16, 21, 0, 0, tzinfo=pytz.UTC), record.get_time()
+        )
+        self.assertEqual(10.0, record.get_price())
+        self.assertEqual(3.0, record.get_amount())
 
-    def test_to_dict(self):
-        record = Record(3, 20.0)
-        self.assertEqual(20.0, record.to_dict()["energy_price"])
+    def test_update(self):
+        record = self.record
+        record.update(price=5.0)
+        self.assertEqual(5.0, record.get_price())
+
+    def test_to_json(self):
+        expected = (
+            """{"time": "2022-12-16T21:00:00+00:00", "price": 10.0, "amount": 3.0}"""
+        )
+        json = self.record.to_json()
+        self.assertEqual(expected, json)
