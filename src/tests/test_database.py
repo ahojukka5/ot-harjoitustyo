9a09efd79a0b695bb8570d26ad2763ec83799d5f
Jukka Aho <ahojukka5@gmail.com>
2022-12-16 14:38:06 +0200

    Fix tests for database

--- src/tests/test_database.py
+++ src/tests/test_database.py
@@ -1,4 +1,5 @@
 import unittest
+from dateutil import parser
 from saehaekkae import Database, Record
 
 import pandas as pd
@@ -7,7 +8,7 @@ import pandas as pd
 class TestDatabase(unittest.TestCase):
     def test_database(self):
         db = Database()
-        record = Record(0, 20.0)
+        record = Record("2022-12-16 21:00:00", 20.0, 3.0)
         db.add_record(record)
         records = db.get_records()
         first_record = records[0]
@@ -15,12 +16,13 @@ class TestDatabase(unittest.TestCase):
 
     def test_to_dataframe(self):
         db = Database()
-        db.add_record(Record(pd.to_datetime("2022-11-28T00:00:00"), 10.0))
-        db.add_record(Record(pd.to_datetime("2022-11-28T01:00:00"), 20.0))
+        db.add_record(Record("2022-11-28T00:00:00", 10.0))
+        db.add_record(Record("2022-11-28T01:00:00", 20.0))
         df = db.to_dataframe()
         print(df)
         row1 = df.iloc[0]
         row2 = df.iloc[1]
         self.assertEqual(2, len(df))
-        self.assertEqual(10.0, row1["energy_price"])
-        self.assertEqual(pd.to_datetime("2022-11-28T01:00:00"), row2.name)
+        self.assertEqual(10.0, row1["price"])
+        print("type of row2.name is ", type(row2.name))
+        self.assertEqual(pd.Timestamp("2022-11-28T01:00:00Z"), row2.name)
