5f5bc8e23c2c6ba2cd758fb3a6d1436e5624bdd9
Jukka Aho <ahojukka5@gmail.com>
2022-12-16 14:37:39 +0200

    Fix test, energy_price -> price

--- src/tests/test_fetch_energy_price.py
+++ src/tests/test_fetch_energy_price.py
@@ -9,7 +9,7 @@ class TestRecord(unittest.TestCase):
         data = fetch_energy_price("test").to_dataframe()
         first_row = data.iloc[0]
         print(first_row)
-        self.assertEqual(0.1563, first_row["energy_price"])
+        self.assertEqual(0.1563, first_row["price"])
         print(first_row.index)
         print(first_row.name)
         self.assertEqual(pd.Timestamp("2022-11-28T00:00:00+02:00"), first_row.name)
