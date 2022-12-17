ed14a6c908003fe5aabc1b2ffaa45a9d30d02952
Jukka Aho <ahojukka5@gmail.com>
2022-12-17 15:09:30 +0200

    Use python-dovenv to help with app configs

--- src/config.py
+++ src/config.py
@@ -5,19 +5,27 @@ defaults are provided.
 """
 
 import os
+from dotenv import load_dotenv
 
-DB_FILE = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "db.csv"))
+root_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+env_file = os.path.join(root_dir, ".env")
+
+if os.path.exists(env_file):
+    load_dotenv(dotenv_path=env_file)
+
+
+def _getenv(env_name, default, prefix="SAEHAEKKAE"):
+    """Return prefixed environment variable value or default value."""
+    return os.getenv("%s_%s" % (prefix, env_name)) or default
+
+
+DB_FILE = _getenv("DB_FILE", "db.csv")
 
 # Options, "test", "local", "internet"
-ENERGY_PRICE_SOURCE = "test"
-ENERGY_PRICE_URI = "https://api.spot-hinta.fi/TodayAndDayForward"
-ENERGY_PRICE_FILE = "data/energy-prices.json"
+ENERGY_PRICE_SOURCE = _getenv("ENERGY_PRICE_SOURCE", "test")
+ENERGY_PRICE_URI = _getenv("PRICE_URI", "https://api.spot-hinta.fi/TodayAndDayForward")
+ENERGY_PRICE_FILE = _getenv("PRICE_FILE", "data/prices.json")
 
-ENERGY_CONSUMPTION_SOURCE = "test"
-ENERGY_CONSUMPTION_FILE = "data/energy-consumption.csv"
-
-if "SAEHAEKKAE_ENERGY_CONSUMPTION_FILE" in os.environ:
-    ENERGY_CONSUMPTION_FILE = os.environ["SAEHAEKKAE_ENERGY_CONSUMPTION_FILE"]
-
-if "SAEHAEKKAE_ENERGY_PRICE_FILE" in os.environ:
-    ENERGY_PRICE_FILE = os.environ["SAEHAEKKAE_ENERGY_PRICE_FILE"]
+# Options, "test", "local"
+ENERGY_CONSUMPTION_SOURCE = _getenv("ENERGY_CONSUMPTION_SOURCE", "test")
+ENERGY_CONSUMPTION_FILE = _getenv("ENERGY_CONSUMPTION_FILE", "data/consumption.csv")
