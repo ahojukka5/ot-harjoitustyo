76313e4efd704d653d98bf64871d6cdf74a902c1
Jukka Aho <ahojukka5@gmail.com>
2022-12-19 23:17:41 +0200

    DateTimePicker: implement basic functionality

--- src/services/datetimepicker.py
+++ src/services/datetimepicker.py
@@ -1,12 +1,27 @@
 from entities import Selection
 
+import dateutil.parser
+import datetime
+
 
 class DateTimePicker:
     def __init__(self):
-        self._selections = []
+        self._selection = Selection()
 
     def pick(self, start):
-        self._selections.append(Selection(start))
+        if isinstance(start, str):
+            start = dateutil.parser.parse(start)
+        end = start + datetime.timedelta(hours=1)
+        self._selection.add_timerange(start, end)
+        return self
 
-    def get_selections(self):
-        return self._selections
+    def pick_between(self, start, end):
+        if isinstance(start, str):
+            start = dateutil.parser.parse(start)
+        if isinstance(end, str):
+            end = dateutil.parser.parse(end)
+        self._selection.add_timerange(start, end)
+        return self
+
+    def to_selection(self):
+        return self._selection
