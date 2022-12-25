1fd9fcc8df8df22424b5c45e44de27b672f65162
Jukka Aho <ahojukka5@gmail.com>
2022-12-25 21:07:11 +0200

    Add unit test for PriceSource

--- src/tests/test_sources.py
+++ src/tests/test_sources.py
@@ -1,8 +1,26 @@
 import tempfile
 import unittest
 import json
+from entities.sources import PriceSource
 from entities.sources import GenericSource
 from repositories import Database
+
+
+class TestPriceSource(unittest.TestCase):
+    def test_source(self):
+        data = [
+            {
+                "Rank": 23,
+                "DateTime": "2022-12-26T19:00:00+02:00",
+                "PriceNoTax": 1.0,
+                "PriceWithTax": 1.22,
+            }
+        ]
+        db = Database()
+        source = PriceSource(db, data=data)
+        source.update()
+        print(db.get_records())
+        self.assertEqual(1.0, db.get_record("2022-12-26 17:00:00").get_price())
 
 
 class TestGenericSource(unittest.TestCase):
@@ -16,5 +34,4 @@ class TestGenericSource(unittest.TestCase):
         tf.seek(0)
         source = GenericSource(db, local_file=tf.name)
         source.update()
-        print(db.get_records())
         self.assertEqual(1.0, db.get_record("2022-12-21 22:00:00").get_price())
